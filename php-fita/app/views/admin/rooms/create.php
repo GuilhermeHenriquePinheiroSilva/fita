b411de55d390368620d14b104608ac6b45bcaf92
GUILHERME  HENRIQUE PINHEIRO DA SILVA <0221432411021@fatectq.local>
2025-11-04 08:40:11 -0300

    folderFix

--- php-fita/app/views/admin/rooms/create.php
+++
@@ -1,8 +0,0 @@
-<?php // app/views/admin/rooms/create.php ?>
-<h1>Criar Sala</h1>
-<form method="post" action="/admin/rooms/create">
-    <label>Nome: <input name="name" required /></label><br/>
-    <label>Capacidade: <input name="capacity" type="number" /></label><br/>
-    <label>Local: <input name="location" /></label><br/>
-    <button type="submit">Criar</button>
-</form>
